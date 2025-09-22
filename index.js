fce8cb80e221c0208507966889c9e71beef91645
Artur <divermsurartur@gmail.com>
2025-09-22 14:11:36 +0200

    Terminado

--- index.js
+++ index.js
@@ -36,4 +36,54 @@ for (let i = 0; i < 11; i++){
 }
 console.log(postI);
 
+let sumaPAres = 0;
+for(let i = 0; i < 10; i++){
+    if(i % 2 == 0){
+        sumaPares += i
+    }else{
+        "No es numero par";
+    }
+}
+
+//Variables.
+let variableValorNumerico = 5;
+const miNombre = "Artur";
+const miNumeroFav = 7;
+
+//Booleanos.
+
+let booleanOr = booleano1 || booleano2;
+let booleanoMix1 = (booleano1 && 
+TAU/2 === PI) || (variableValorNumerico >= miNumeroFav);
+
+let seisNoEsNueve = 6 !== 9;
+let booleanoMix2 = 
+variableValorNumerico => 1 || variableValorNumerico < -(miNumeroFav * TAU);
+
+//Operadores
+valorSuma = miNumeroFav + variableValorNumerico;
+valorResta = miNumeroFav - variableValorNumerico;
+valorMultiplicacion = miNumeroFav * variableValorNumerico;
+valorDivision = miNumeroFav / 3;
+
+//Bucles
+let contarHasta10 = 0;
+while(contarHasta10 < 10){
+    contarHasta10++;
+}
+
+let preI = 0;
+let preJ = 0;
+
+for(let i = 0; i < 11; i++){
+    preI += ++preJ;
+}
+
+let sumaImpares = 0;
+for(let i = 0; i < 10; i++){
+    if(i % 2 !== 0) {
+        sumaImpares += i;
+    } // Si es diferente a 0 que sume.
+}
+console.log("La suma de impares son:", sumaImpares);
 
